04c1d358fac6a791a6202339dea79efc93b3ec72
syedbarimanjan <syedbarimanjan@gmail.com>
2023-06-12 09:48:00 +0500

    sorting the games

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector"
 export interface GameQuery{
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder:string
 }
 function App() {
   const [gameQuery,setGameQuery]=useState<GameQuery>({} as GameQuery)
@@ -27,7 +28,7 @@ function App() {
     <GridItem area='main'>
       <HStack spacing={5} marginBottom={5} paddingLeft={2}>
         <PlatformSelector selectedPLatform={gameQuery.platform} onSelectPlatform={(platform)=> setGameQuery({...gameQuery,platform})}/>
-        <SortSelector/>
+        <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery,sortOrder})}/>
       </HStack>
       <GameGrid gameQuery={gameQuery}/>
     </GridItem>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,17 +1,27 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react"
 import { BsChevronDown } from "react-icons/bs"
 
-const SortSelector = () => {
+interface Props{
+    onSelectSortOrder:(sortOrder:string) => void;
+    sortOrder:string
+}
+const SortSelector = ({onSelectSortOrder,sortOrder}:Props) => {
+    const sortOrders=[
+        {value:"",label:"Relevence"},
+        {value:"-added",label:"Date added"},
+        {value:"name",label:"Name"},
+        {value:"-released",label:"Release date"},
+        {value:"-metacritic",label:"Popularity"},
+        {value:"-rating",label:"Average rating"},
+    ]
+    const currentSortOrder=sortOrders.find(order => order.value === sortOrder);
     return (
         <Menu>
-            <MenuButton  as={Button} rightIcon={<BsChevronDown/>}>order by : relevence</MenuButton>
+            <MenuButton  as={Button} rightIcon={<BsChevronDown/>}>Order by: {currentSortOrder?.label || "Relevence"}</MenuButton>
             <MenuList>
-                <MenuItem>a</MenuItem>
-                <MenuItem>b</MenuItem>
-                <MenuItem>c</MenuItem>
-                <MenuItem>d</MenuItem>
-                <MenuItem>e</MenuItem>
-                <MenuItem>f</MenuItem>
+                {sortOrders.map(order=>(
+                    <MenuItem onClick={()=>{onSelectSortOrder(order.value)}} key={order.value} value={order.value}>{order.label}</MenuItem>
+                ))}
             </MenuList>
         </Menu>
     )
