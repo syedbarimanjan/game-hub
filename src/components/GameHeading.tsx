b1de77bcfd554b00c1b181c93ad0e3ba30463666
syedbarimanjan <syedbarimanjan@gmail.com>
2023-06-13 11:18:38 +0500

    cleaning up the genres

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -7,7 +7,7 @@ interface Props{
 const GameHeading = ({gameQuery}:Props) => {
     const heading=`${gameQuery.platform?.name || ""} ${gameQuery.genre?.name || ""} Games`
     return (
-        <Heading as="h1" marginBottom={5}>{heading}</Heading>
+        <Heading as="h1" marginBottom={5} fontSize="5xl">{heading}</Heading>
     )
 }
 
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem,Text } from '@chakra-ui/react'
+import { Button, HStack, Heading, Image, List, ListItem,Text } from '@chakra-ui/react'
 import useGenre, { Genre } from '../hooks/useGenre'
 import getCroppedImageUrl from '../services/image-url'
 import GenreListSkeleton from './GenreListSkeleton';
@@ -14,6 +14,7 @@ const GenreList = ({selectedGenre,onSelectGenre}:Props) => {
     if(error) return null
     return (
       <>
+        <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
         {isLoading && skeletons.map((skeleton)=>(
           <GenreListSkeleton key={skeleton}/>
         ))}
@@ -21,8 +22,8 @@ const GenreList = ({selectedGenre,onSelectGenre}:Props) => {
             {data?.map(genre => (
                 <ListItem key={genre.id} paddingY="5px">
                   <HStack>
-                    <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-                    <Button fontWeight={genre.id === selectedGenre?.id ? "bold":"normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
+                    <Image boxSize="32px" borderRadius={8} objectFit="cover" src={getCroppedImageUrl(genre.image_background)}/>
+                    <Button whiteSpace="normal" textAlign="left" fontWeight={genre.id === selectedGenre?.id ? "bold":"normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
                   </HStack>
                 </ListItem>
             ))}
